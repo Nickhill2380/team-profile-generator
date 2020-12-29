1838f1a8038ed0fd56c22907ae4dba8d0f0027b7
Nick Hill <nickhill2380@gmail.com>
2020-12-28 19:08:40 -0500

    started employee class, test passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates a employee object', () => {
+    const employee = new Employee('Andy', 1 ,'email@website.com');
+
+    expect(employee.name).toBe('Andy');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('@'));
+});
